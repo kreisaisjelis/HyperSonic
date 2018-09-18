b3889bee2f577c0dd66938bf1d7f9a38844adc7d
kreisaisjelis <kreisaisjelis@gmail.com>
2018-09-18 23:43:47 +0300

    Added error/debug stream to file

--- shell.py
+++ shell.py
@@ -2,8 +2,10 @@ import subprocess
 
 myinput = open('input.txt')
 myoutput = open('output.txt', 'w')
+errorstr = open('errors.txt', 'w')
 
-completed = subprocess.run(['python3', 'hyper.py'], stdin = myinput, stderr=subprocess.DEVNULL, stdout= myoutput )
+completed = subprocess.run(['python', 'hyper.py'], stdin = myinput, stderr=errorstr, stdout= myoutput )
 print('returncode:', completed.returncode)
 
 myoutput.flush()
+errorstr.flush()
