9c323323add57a2f4192abbbead7647030c00611
kreisaisjelis <kreisaisjelis@gmail.com>
2018-09-20 00:00:56 +0300

    File input passed into unit tests

--- unitTests.py
+++ unitTests.py
@@ -1,14 +1,22 @@
 from hyper import GameState
 from hyper import GlobalState
 import sys
+import filecmp
 
-gameState = GameState(GlobalState().LoadGlobals())
-gameState.ReadField()
-gameState.ReadEntities()
-gameState.ChainBombs()
-gameState.PredictExplosions()
-bomb_target,next_move = gameState.GetNewTarget(True)
+with open('unitTest1.out', 'w') as f, open('unitTest1.in') as fIn:
+	sys.stderr = f
+	sys.stdin = fIn
 
-print(f'Reachables {gameState.Reachable((1,1),3)}', file=sys.stderr)
-print(f'Bombs {gameState.bombs}', file=sys.stderr)
-print(f'Danger field {gameState.PredictExplosions()}', file=sys.stderr)
+
+	gameState = GameState(GlobalState().LoadGlobals())
+	gameState.ReadField()
+	gameState.ReadEntities()
+	gameState.ChainBombs()
+	gameState.PredictExplosions()
+	bomb_target,next_move = gameState.GetNewTarget(True)
+
+	print(f'Reachables {gameState.Reachable((1,1),3)}', file=sys.stderr)
+	print(f'Bombs {gameState.bombs}', file=sys.stderr)
+	print(f'Danger field {gameState.PredictExplosions()}', file=sys.stderr)
+
+print(filecmp.cmp('unitTest1.out','unitTest1.exp', False))
